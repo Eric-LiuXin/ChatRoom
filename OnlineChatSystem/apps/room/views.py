f1800e79c7c046b984f4f0a0b6557de8f8e1718c
Eric-LiuXin <xin.liu@forceclouds.com>
2019-08-28 18:24:53 +0800

    项目初始化

---
+++ OnlineChatSystem/apps/room/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+# Create your views here.
+
+
+def index(request):
+    return JsonResponse({"success": True})
